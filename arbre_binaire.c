3f036b1350fd82fa667ac4360bdcae1dac92ea80
AlexisBCD <alexisuphf@gmail.com>
2021-11-25 11:42:42 +0100

    Ajout de insérer dans arbre_binaire.c

--- arbre_binaire.c
+++ arbre_binaire.c
@@ -1,20 +1,23 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-typedef struct noeud{
+typedef struct noeud
+{
     int id;
     int numero;
     char *nom;
     char *prenom;
     struct noeud *right;
     struct noeud *left;
-}Noeud;
+} Noeud;
 
-typedef Noeud * Arbre;
+typedef Noeud *Arbre;
 
-Arbre creerNoeud(int id, int numero, char *nom, char *prenom) {
-    Arbre nouveau = (Arbre) malloc(sizeof(Noeud));
-    if (nouveau) {
+Arbre creerNoeud(int id, int numero, char *nom, char *prenom)
+{
+    Arbre nouveau = (Arbre)malloc(sizeof(Noeud));
+    if (nouveau)
+    {
         nouveau->id = id;
         nouveau->numero = numero;
         nouveau->nom = nom;
@@ -24,6 +27,77 @@ Arbre creerNoeud(int id, int numero, char *nom, char *prenom) {
     return nouveau;
 }
 
-Arbre inserer(int id, int numero, char *nom, char *prenom, Arbre a){
+void afficherParOrdreCroissant(Arbre a)
+{
+    if (a != NULL)
+    {
+        if (a->left != NULL)
+        {
+            afficherParOrdreCroissant(a->left);
+        }
+        printf("Id: %d\nNom: %s\nPrenom: %s\nnumero de telephone: %d\n\n", a->id, a->nom, a->prenom, a->numero);
+        if (a->right != NULL)
+        {
+            afficherParOrdreCroissant(a->right);
+        }
+    }
+}
 
+void afficherParOrdreDecroissant(Arbre a)
+{
+    if (a != NULL)
+    {
+        if (a->right != NULL)
+        {
+            afficherParOrdreDecroissant(a->right);
+        }
+        printf("Id: %d\nNom: %s\nPrenom: %s\nnumero de telephone: %d\n\n", a->id, a->nom, a->prenom, a->numero);
+        if (a->left != NULL)
+        {
+            afficherParOrdreDecroissant(a->left);
+        }
+    }
+}
+
+void afficherArbre(Arbre a, int nv)
+{
+    int i;
+    if (a != NULL)
+    {
+        afficherArbre(a->right, nv + 1);
+
+        for (i = 1; i <= nv; i++)
+        {
+            printf("  ");
+        }
+        printf("%d\n", a->id);
+        afficherArbre(a->left, nv + 1);
+    }
+}
+
+
+Arbre inserer(Arbre a, int id, int numero, char *nom, char *prenom)
+{
+    if (a == NULL)
+    {
+        return creerNoeud(id, numero,nom, prenom);
+    }
+    else if (id < a->id)
+    {
+        if(a->left != NULL){
+            inserer(a->left, id, numero, nom, prenom);
+        }
+        else{
+            a->left = creerNoeud(id, numero,nom, prenom);
+        }
+    }
+    else if (id > a->id)
+    {
+        if(a->right != NULL){
+            inserer(a->right, id, numero, nom, prenom);
+        }
+        else{
+            a->right = creerNoeud(id, numero, nom, prenom);
+        }
+    }
 }
