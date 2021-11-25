9c6b8efa689a1950951efb41fa0263a6562195ad
Mathis <69654739+AventeL@users.noreply.github.com>
2021-11-25 10:04:30 +0100

    Afficher (#2)
    * Ajout fonction d'affichage de l'arbre binaire
    
    * clear main
    
    * mise a jour arbre_binaire.h

---
+++ arbre_binaire.h
@@ -0,0 +1,10 @@
+#include "arbre_binaire.c"
+
+void afficherParOrdreCroissant(Arbre a);
+
+void afficherParOrdreDecroissant(Arbre a);
+
+void afficherArbre(Arbre a, int nv);
+
+
+
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "stdlib.h"
 #include "arbre_binaire.c"
 
-void main(){
-    Arbre a;
+void main()
+{
+    
 }
